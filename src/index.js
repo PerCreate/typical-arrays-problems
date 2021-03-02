b8c40161c1bff940d5a2075160a38ddf892fdc2b
PerCreate <kostya.pershin007@gmail.com>
2021-03-02 15:55:37 +0400

    feat: add new functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let result = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        let temp = result;
+        result = array[i] < temp ? array[i] : temp;
+    }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let result = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        let temp = result;
+        result = array[i] > temp ? array[i] : temp;
+    }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let result = 0;
+    let counter = array.length;
+    for (let i = 0; i < array.length; i += 1) {
+        result += array[i];
+    }
+  return result / counter;
 }
